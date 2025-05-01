90adcaf6bc68cca6cfd1f116c57ecbde60ff9b41
Rosalie337 <rosalie337@gmail.com>
2025-05-01 12:04:37 -0700

    created the string for the madlib.

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 # This is my first attempt at creating a python project.
 # I will create input variables, a string and call those variables.
 
-example = input("Enter a word: ")
+example = input(" ")
+
+madlibs = ("Welcome to Portland, Oregon {name}."
+           "I hope you are ready for the {adjective} culture you are going to take in."
+           "There are mountains and {noun2}. You can hike, {verb},"
+           "and bike all over the city. Make sure to not miss out on the"
+           "opportunities to {verb2} around the our {adjective2} city.")
+
 
 print(example)
