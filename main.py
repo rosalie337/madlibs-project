f642c096be02b69802d357e5cd0527a4a06f63dd
Rosalie337 <rosalie337@gmail.com>
2025-05-01 12:09:45 -0700

    Created input variables and corrected the madlib string variables.

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 # This is my first attempt at creating a python project.
 # I will create input variables, a string and call those variables.
 
-example = input(" ")
+name = input("Name: ")
+adjective = input("Adjective: ")
+noun = input("Noun: ")
+verb = input("Verb: ")
+verb2 = input("Verb: ")
+adjective2 = input("Adjective: ")
 
 madlibs = ("Welcome to Portland, Oregon {name}."
            "I hope you are ready for the {adjective} culture you are going to take in."
-           "There are mountains and {noun2}. You can hike, {verb},"
+           "There are mountains and {noun}. You can hike, {verb},"
            "and bike all over the city. Make sure to not miss out on the"
            "opportunities to {verb2} around the our {adjective2} city.")
 
