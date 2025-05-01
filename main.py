83381ee6f316d67e3f3725f22c5376a0005aa997
Rosalie337 <rosalie337@gmail.com>
2025-05-01 15:53:52 -0700

    Cleaned up file.

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# This is my first attempt at creating a python project.
-# I will create input variables, a string and call those variables.
-
 name = input("Name: ")
 adjective = input("Adjective: ")
 noun = input("Plural Noun: ")
